7c09a06f9ff9fb11597cee825c1fd7ef820a1336
ofirer92 <ofir.schw@gmail.com>
2021-02-11 14:24:08 +0200

    newon

---
+++ src/myTestApp/quiz_app/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  void answerQ() {
+    print('clicked');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var qoustion = ["what is your favorite color", "what isyou pets name"];
+    var qustion_num = 0;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("wallak"),
+        ),
+        body: Column(
+          children: [
+            Text(qoustion[qustion_num]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () {
+                answerQ();
+              },
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQ,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQ,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
